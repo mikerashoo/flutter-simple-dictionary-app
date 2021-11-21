6f8c3dff617ef3723b8c88242d0dca1900282612
Mikias Birhanu <mkbirhanu@gmail.com>
2021-11-21 02:06:22 -0800

    dependencies and dictionary data model added

---
+++ lib/models/dictionary.dart
@@ -0,0 +1,15 @@
+class Dictionary {
+  final int id;
+  final String kafinoonoo;
+  final String amharic;
+  final String english;
+  final String type;
+
+  Dictionary({
+    required this.id,
+    required this.kafinoonoo,
+    required this.amharic,
+    required this.english,
+    required this.type,
+  });
+}
